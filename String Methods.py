e7864e911b89e4e8505d26285e516ad7eb8655e0
LuvBugga <luvbugga2006@gmail.com>
2026-04-21 11:41:08 +0530

    Program 3

---
+++ String Methods.py
@@ -0,0 +1,9 @@
+line = "Luv is a Pro in Minecraft"
+print(line.upper())
+print(line.lower())
+print(line.title())
+print(line.capitalize())
+print(len(line))
+print("Luv" in line)
+print(line.replace("Luv","Kush"))
+print(line.find("Pro"))
